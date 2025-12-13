f238e242b514e1410fee14d84f34c57ab54a3181
Daniel <danielviana2127@yahoo.com>
2025-12-13 16:29:50 -0300

    style: remove linhas em branco finais (flake8)

--- src/test_app.py
+++ src/test_app.py
@@ -1,12 +1,15 @@
 import unittest
+
 from app import somar
 
+
 class TestApp(unittest.TestCase):
+
     def test_somar(self):
         self.assertEqual(somar(2, 3), 5)
         self.assertEqual(somar(-1, 1), 0)
         self.assertEqual(somar(0, 0), 0)
 
+
 if __name__ == "__main__":
     unittest.main()
-
